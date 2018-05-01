cd4491d7664dd4adfff57e5d3091dd4587d26447
Hideyuki Numaoka <kise1112.h@gmail.com>
2018-05-01 11:41:26 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 'use strict';
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
+    if(memo.has(n)) { //.hasはmemoを参照して(n)のkey値を持ってるかどうかでtrueかfalseを返す
+        return memo.get(n); //もし持っていたらそれを返して終了する
     }
-    return fib(n - 1) + fib(n - 2);
+    const value = fib(n - 1) + fib(n - 2); //なければ計算をする
+    memo.set(n, value);　// 計算結果を連想配列に追加する
+    return value; //その値を返す
 }
+
 const length = 40;
 for (let i = 0; i <= length; i++) {
     console.log(fib(i));
